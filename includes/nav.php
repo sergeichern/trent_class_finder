ec4b5506f0f3b6a537ea9f741073c1ce3a23f462
sergeichern <sergeiche01@gmail.com>
2025-09-10 00:47:17 -0400

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -4,7 +4,7 @@
         <a href="index.php">Home</a>
         <a href="map.php">See Building Maps</a>
         <a href="bathroom.php">Find the Nearest Bathroom</a>
-        <a href="professor.php">Professor's offices</a>
-        <a href="restaurant.php">Restaurant</a>
+        <!--   <a href="professor.php">Professor's offices</a>
+        <a href="restaurant.php">Restaurant</a> -->
     <span>
 </nav>
